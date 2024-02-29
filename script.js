6a1187f6c44000cd519e04672fa69b8b1ff78d6c
Karan Sayambar <86702398+Karansayambar@users.noreply.github.com>
2024-02-29 10:05:08 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,28 @@
 //your JS code here. If required.
+let ageInput = document.getElementById("age");
+let nameInput = document.getElementById("name");
+let button = document.getElementById("btn");
+
+
+button.addEventListener("click", (e) => {
+	  let age = parseInt(ageInput.value);
+      let name = nameInput.value;
+	e.preventDefault();
+	let promise = new Promise((resolve, reject) => {
+		setTimeout(() => {
+			if(age > 18){
+				resolve(`Welcome, ${name}. You can vote.`)
+			}else{
+				reject(`Oh sorry ${name}. You aren't old enough.`)
+			}
+		},4000)
+	});
+	promise
+	.then((message) => {
+	alert(message);
+})
+	.catch((err) => {
+	alert(err);
+})
+})
+
